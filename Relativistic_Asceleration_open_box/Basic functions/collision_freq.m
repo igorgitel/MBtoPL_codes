fd02b820c21eaf33e74a0f46877064ac7b3ea456
igorgitel <39522592+igorgitel@users.noreply.github.com>
2025-07-13 17:57:02 +0300

    Add files via upload

---
+++ Relativistic_Asceleration_open_box/Basic functions/collision_freq.m
@@ -0,0 +1,17 @@
+function f_now = collision_freq(p1,e1,p2,e2,alpha)
+
+% Calculation the colision frequamcy
+v1=p1./e1;
+v2=p2./e2;
+
+Cr=cross(v1,v2);
+Res=v1-v2;
+[e_rel,p_rel] = LorentzTransform(e1,p1,v2);
+v_rel=abs(p_rel)/e_rel;
+sig=sigma(v_rel,alpha);
+% sig=1;
+f_now=sig*sqrt(dot(Res,Res)-dot(Cr,Cr));
+if ~isreal(prod(f_now))
+    error('f isnt real');
+end
+end
---
+++ Relativistic_Asceleration_open_box/Basic functions/low_err_histlog.m
@@ -0,0 +1,30 @@
+function [bins,fsim10,fsim50,fsim100,f_hist] = low_err_histlog(data,edges)
+    %[f_sim0,edges] = histcounts(data,edges,'Normalization','countdensity');
+    [f_sim0,edges] = histcounts(data,edges);
+    f_hist=f_sim0;
+    Le=log10(edges);
+    centers=zeros(1,length(edges)-1);
+
+    for i=1:length(edges)-1
+        centers(i)=(Le(i)+Le(i+1))/2;
+        if f_sim0(i)<10
+            fsim10(i)=0;
+            fsim50(i)=0;
+            fsim100(i)=0;
+        else 
+            fsim10(i)=f_sim0(i)/(edges(i+1)-edges(i));
+            if f_sim0(i)<50
+                fsim50(i)=0;
+                fsim100(i)=0;
+            else
+                fsim50(i)=f_sim0(i)/(edges(i+1)-edges(i));
+                if f_sim0(i)<100
+                    fsim100(i)=0;
+                else
+                    fsim100(i)=f_sim0(i)/(edges(i+1)-edges(i));
+                end
+            end
+        end
+    end
+    bins=10.^(centers);
+end
