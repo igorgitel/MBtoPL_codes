9c4364c2a797d37236811ea9bc9b91442452a1bb
igorgitel <39522592+igorgitel@users.noreply.github.com>
2025-07-14 14:03:33 +0300

    Update func_One_type_prep.m
    icb->ic initial condition struct rename

--- single_mass_close_box/func_One_type_prep.m
+++ single_mass_close_box/func_One_type_prep.m
@@ -1,4 +1,4 @@
-function [Enb,data,icb]=func_One_type_prep(m,gb,N,step_inc)
+function [Enb,data,ic]=func_One_type_prep(m,gb,N,step_inc)
 addpath('Basic functions/')
 %% initial condition
 
@@ -6,16 +6,16 @@ addpath('Basic functions/')
 b=sqrt(gb^2/(1+gb^2));
 g=1/sqrt(1-b^2);
 
-icb.m=m;
-icb.e=m*g;
-icb.N=N;
-icb.ke=icb.e-icb.m;
-icb.edges=10.^(-16:0.1:16);
+ic.m=m;
+ic.e=m*g;
+ic.N=N;
+ic.ke=ic.e-ic.m;
+ic.edges=10.^(-16:0.1:16);
 %% prepare initial energies
-Enb=icb.e*ones(1,icb.N);
+Enb=ic.e*ones(1,ic.N);
 syms x
-temp=3/2*(icb.e-m);
-T = double(vpasolve(3*x+icb.m*besselk(1,icb.m/x)/besselk(2,icb.m/x)==icb.e,x,temp));
+temp=3/2*(ic.e-m);
+T = double(vpasolve(3*x+ic.m*besselk(1,ic.m/x)/besselk(2,ic.m/x)==ic.e,x,temp));
 clear x
 %% time
 t=0;
@@ -41,17 +41,17 @@ tic
 while t<max_time
     trys=trys+1;
     % index selection
-    id1=randi(icb.N);
-    id2=randi(icb.N);
+    id1=randi(ic.N);
+    id2=randi(ic.N);
     while id2==id1
-        id2=randi(icb.N);
+        id2=randi(ic.N);
     end
 
     % colision prepare
     e1=Enb(id1);
     e2=Enb(id2);
-    p1=sqrt((Enb(id1))^2-icb.m^2)*dir_mat(ind_dir,:);
-    p2=sqrt((Enb(id2))^2-icb.m^2)*dir_mat(ind_dir+1,:);
+    p1=sqrt((Enb(id1))^2-ic.m^2)*dir_mat(ind_dir,:);
+    p2=sqrt((Enb(id2))^2-ic.m^2)*dir_mat(ind_dir+1,:);
     ind_dir=mod(ind_dir+2,N);
     if ind_dir==0
         ind_dir=1;
@@ -60,7 +60,7 @@ while t<max_time
 
     f_max = max([f_now f_max]);
 
-    t=t+1/(icb.N/2)*(1/f_max);
+    t=t+1/(ic.N/2)*(1/f_max);
     if f_now > f_max*rand
         coll_num=coll_num+1;
         n0=dir_mat(ind_dir+1,:);
@@ -72,22 +72,22 @@ while t<max_time
         [Enb(id1), ~,Enb(id2), ~] = RelCol(e1, p1,e2, p2, n0);
     end
     if t>step
-        data(idx).kin_energy=mean(Enb-icb.m);
-        [data(idx).bins_Ek,data(idx).f_sim_Ek] = histlog(Enb-icb.m,icb.edges);
-        data(idx).f_sim_Ek=data(idx).f_sim_Ek/icb.N;
-        data(idx).Teo_Ek=MJDF(data(idx).bins_Ek,icb.m,T);
+        data(idx).kin_energy=mean(Enb-ic.m);
+        [data(idx).bins_Ek,data(idx).f_sim_Ek] = histlog(Enb-ic.m,ic.edges);
+        data(idx).f_sim_Ek=data(idx).f_sim_Ek/ic.N;
+        data(idx).Teo_Ek=MJDF(data(idx).bins_Ek,ic.m,T);
         
-        mu=sqrt((Enb./icb.m).^2-1);
-        [data(idx).bins_mu,data(idx).f_sim_mu] = histlog(mu,icb.edges);
-        data(idx).f_sim_mu=data(idx).f_sim_mu/icb.N;
-        data(idx).Teo_mu=MJmu(data(idx).bins_mu,icb.m,T);
+        mu=sqrt((Enb./ic.m).^2-1);
+        [data(idx).bins_mu,data(idx).f_sim_mu] = histlog(mu,ic.edges);
+        data(idx).f_sim_mu=data(idx).f_sim_mu/ic.N;
+        data(idx).Teo_mu=MJmu(data(idx).bins_mu,ic.m,T);
         
         data(idx).coll_num=coll_num/N;
         data(idx).time=t;
         disp({'ts' 'mean energy' 'trys_c' 'coll_num_c'; ...
             t (data(idx).kin_energy) trys-temp1 coll_num-temp2;...
             'f_max' 'max_e' 'min_e' 'ind_dir' ;...
-            f_max max(Enb-icb.m) min(Enb-icb.m) ind_dir;})
+            f_max max(Enb-ic.m) min(Enb-ic.m) ind_dir;})
         temp1=trys;
         temp2=coll_num;
         step=step+step_inc;
@@ -103,5 +103,5 @@ str=['One_type_mass_' num2str(m,'%1.0e')...
     '_initial_gb_' num2str(gb,'%1.0e')...
     '_particle_number_' num2str(N,'%1.0e')];
 % anima_s(data,str,'Videos1');
-save(fullfile("saves",[str '.mat']),'Enb',"data","icb");
+save(fullfile("saves",[str '.mat']),'Enb',"data","ic");
 end
